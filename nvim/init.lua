9ed462dcef26fc0d397d3682c2a93e66f46ef982
aryan <paryan@digitalocean.com>
2024-09-15 16:00:46 +0200

    Start fresh

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+--
+--                            ⢸⣦⡈⠻⣿⣿⣿⣶⣄
+--                            ⢸⣿⣿⣦⡈⠻⣿⣿⣿⣷⣄
+--                      ⣀⣀⣀⣀⣀⣀⣼⣿⣿⣿⣿ ⠈⠻⣿⣿⣿⣷⣄
+--                      ⠈⠻⣿⣿⣿⣿⣿⡿⠿⠛⠁   ⠈⠻⢿⣿⣿⣷⣄
+--
+-- Personal Neovim configuration of Aryan Ahmed <thearyanahmed@gmail.com>
+
+require('user.options')
+require('user.keymaps')
+require('user.misc')
+require('user.lazy')
+
+-- vim: nospell
