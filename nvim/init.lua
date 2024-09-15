5e9bfaff601e6f6e707116a705d30fe147bc7424
aryan <paryan@digitalocean.com>
2024-09-15 21:50:01 +0200

    Set tab stop to 4

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,6 +10,10 @@ require("core.utils").load_mappings()
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
+vim.opt.tabstop = 4        -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 4     -- Number of spaces to use for autoindent
+vim.opt.expandtab = true   -- Convert tabs to spaces
+
 -- bootstrap lazy.nvim!
 if not vim.loop.fs_stat(lazypath) then
   require("core.bootstrap").gen_chadrc_template()
